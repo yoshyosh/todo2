08eb7c5f6f387f7267f157840f217edb596362f4
= <=>
2020-02-05 18:18:24 +0100

    add postgres

--- app/javascript/packs/todo_list.jsx
+++ app/javascript/packs/todo_list.jsx
@@ -4,8 +4,8 @@ import PropTypes from 'prop-types'
 export default class TodoList extends React.Component {
   render(){
     const data = this.props.data;
-    const listItems = data.map((item) => 
-      <li className={item.completed ? 'complete' : ''}>
+    const listItems = data.map((item) =>
+      <li className={item.completed ? 'complete' : ''} key={item.id}>
         <span>{item.description}</span>
       </li>
     );
